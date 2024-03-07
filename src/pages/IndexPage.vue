2e7bdb067a94b932b885a0da9e5c7e951c6adfe8
kanyaphatyosanit <119926254+kanyaphatyosanit@users.noreply.github.com>
2024-03-07 23:40:47 +0700

    indexpage

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<!-- IndexPage.vue -->
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Vue is awesome.',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
+  
